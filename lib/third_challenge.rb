704b1f9c6cd93ea48c9ef0cea78865848f3c7dfd
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:48:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,20 +11,24 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
