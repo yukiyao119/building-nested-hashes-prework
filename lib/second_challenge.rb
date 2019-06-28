ce9586fb82cff5e2605af11c7883b1160db6f984
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:44:41 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {}
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
