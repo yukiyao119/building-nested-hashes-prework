df9af93916576985d36e95dd809be383963c6530
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:47:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,8 +10,9 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        :name => "Lord Montague",
-        :age => "53"
+        :name => "Romeo",
+        :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
    }, 
