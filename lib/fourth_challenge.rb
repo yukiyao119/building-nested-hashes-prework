e77afb301946c0560e2f12c2187ec710909a545a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:52:45 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,8 +11,8 @@ def fourth_challenge
           :attitude => "worried"
         },
         {
-          :name => "Benvolio",
-          :age => "17",
+          :name => "Mercutio",
+          :age => "18",
           :attitude => "worried"
         }
       ]
