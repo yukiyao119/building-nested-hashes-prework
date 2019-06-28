17878d51e6f18a82498f706fd98e8776b18f38a1
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:52:06 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,8 +6,9 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {
-          :name
+          :name => ""
           :age
+          :attitude
         },
         {
           
