4178d6e77443b842ddd2788d777b401f2a72e77c
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:44:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      
       }, 
     :capulet => {
       #your key/value pairs here!
