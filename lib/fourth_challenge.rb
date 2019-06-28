3a4d07e36e256a1c5af35d695b1133730a05d1e6
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:53:22 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,7 +22,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried" 
+        }
       ]
    }
 }
