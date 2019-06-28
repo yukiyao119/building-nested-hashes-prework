561e4fbd7e5fc7a9c12651142146aab7236957a7
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-28 19:46:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague",
-        :age
+        :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
